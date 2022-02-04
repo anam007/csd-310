cadfeba87a81607fdf53df99bd8c5d769cb071e8
Anam Umanah <anamahaziah@gmail.com>
2022-02-03 20:26:10 -0600

    pytech.update

---
+++ module_6/pytech_update.py
@@ -0,0 +1,39 @@
+#import statement
+from pymongo import MongoClient
+
+# MongoDB connection string
+url = "mongodb+srv://admin:admin@cluster0.sjyfk.mongodb.net/pytech" 
+
+#connect to mongoDB cluster
+client = MongoClient(url)
+
+#connect to pytech database
+db =client.pytech
+
+# get the students collection 
+students = db.students
+
+# find all students in the collection 
+student_list = students.find({})
+
+# display message 
+print("\n  -- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
+
+# loop over the collection and output the results 
+for student_doc in student_list:
+    print("  Student ID: " + student_doc["student_id"] + "\n  First Name: " + student_doc["first_name"] + "\n  Last Name: " + student_doc["last_name"] + "\n")
+
+# update student_id 1007
+result = students.update_one({"student_id": "1007"}, {"$set": {"last_name": "Windslow"}})
+
+# find the updated student document 
+alex = students.find_one({"student_id": "1007"})
+
+# display message
+print("\n  -- DISPLAYING STUDENT DOCUMENT 1007 --")
+
+# output the updated document to the terminal window
+print("  Student ID: " + alex["student_id"] + "\n  First Name: " + alex["first_name"] + "\n  Last Name: " + alex["last_name"] + "\n")
+
+# exit message 
+input("\n\n  End of program, press any key to continue...")
